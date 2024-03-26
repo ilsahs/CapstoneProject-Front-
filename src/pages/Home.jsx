68ff58b2eff578bcede7a37d6cd8d24623da945d
iman600 <149717098+iman600@users.noreply.github.com>
2024-03-26 23:06:09 +0300

    Homepage styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import '../App.css';
 import HeroSection from '../HeroSection'; 
 import Footer from '../Footer'; 
 import Chatbot from '../Chatbot';
+import Trending from '../Trending';
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Trending/>
       <Chatbot/>
       <Footer />
     </>
